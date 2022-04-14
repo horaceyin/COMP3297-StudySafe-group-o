a4407a0500a0c6e9307ce9c30ca471bb17490513
WoodyWong <37654138+WoodyWSL@users.noreply.github.com>
2022-04-15 01:42:41 +0800

    define models and enable models in Django Admin

--- StudySafeCore/admin.py
+++ StudySafeCore/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(HKUMember)
+admin.site.register(Venue)
+admin.site.register(VenueEntryExitRecord)
